37b8d51c84e0f5898b3b961a8529c95388daf9d2
Batul H Suratwala <batulhs2005@gmail.com>
2025-05-15 08:31:26 +0530

    Create (Lab1a)BubbleSort.c

---
+++ (Lab1a)BubbleSort.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include <stdlib.h>
+int main(){
+    int i,j,n,a[100],temp;
+    printf("Enter size");
+    scanf("%d",&n);
+    printf("Enter elements");
+    for (i=0;i<n;i++){
+        scanf("%d",&a[i]);
+        }
+    for (i=0;i<n;i++){
+        for (j=0;j<n-i-1;j++){
+            if (a[j]>a[j+1]){
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+    printf("Sorted array:\n");
+    for (i=0;i<n;i++){
+        printf("%d ",a[i]);
+        }
+}
+
