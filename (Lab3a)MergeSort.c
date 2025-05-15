5bb83c32ed445ed7df456c15f5a7cf81fe2ca0c8
Batul H Suratwala <batulhs2005@gmail.com>
2025-05-15 09:56:01 +0530

    Create (Lab3a)MergeSort.c

---
+++ (Lab3a)MergeSort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    i = 0; j = 0; k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) arr[k++] = L[i++];
+        else arr[k++] = R[j++];
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+int main() {
+    int arr[100],m,i;
+    printf("Enter size of array: ");
+    scanf("%d",&m);
+    printf("Enter array elements: ");
+    for (i=0;i<m;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("Original array:\n");
+    printArray(arr, m);
+    mergeSort(arr, 0, m - 1);
+    printf("Sorted array:\n");
+    printArray(arr, m);
+    return 0;
+}
