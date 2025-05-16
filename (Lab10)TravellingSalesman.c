0664db78df45eae0338c02e9477afcc7cf2d2f5f
Batul H Suratwala <batulhs2005@gmail.com>
2025-05-16 09:55:25 +0530

    Create (Lab10)TravellingSalesman.c

---
+++ (Lab10)TravellingSalesman.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX 16
+
+int dist[MAX][MAX];  
+int dp[1 << MAX][MAX];  
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
+
+int tsp(int mask, int pos, int n) {
+    if (mask == (1 << n) - 1) {  
+        return dist[pos][0];  
+    }
+    
+    if (dp[mask][pos] != -1) {
+        return dp[mask][pos];
+    }
+
+    int ans = INT_MAX;
+
+    for (int city = 0; city < n; city++) {
+        if ((mask & (1 << city)) == 0) {  
+            int newAns = dist[pos][city] + tsp(mask | (1 << city), city, n);
+            ans = min(ans, newAns);
+        }
+    }
+
+    dp[mask][pos] = ans;
+    return ans;
+}
+
+int main() {
+    int n;
+
+    printf("Enter the number of cities: ");
+    scanf("%d", &n);
+
+    printf("Enter the distance matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+        }
+    }
+
+    for (int i = 0; i < (1 << n); i++) {
+        for (int j = 0; j < n; j++) {
+            dp[i][j] = -1;
+        }
+    }
+
+    int result = tsp(1, 0, n);
+    
+    printf("The minimum cost to visit all cities is: %d\n", result);
+
+    return 0;
+}
